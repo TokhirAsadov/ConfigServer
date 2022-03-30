d922b7f4d185d864042584b00736523ed302cb6a
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-03-30 11:43:56 +0500

    Bismillah!.. First Micro Service

---
+++ FirstClient/src/test/java/uz/pdp/firstclient/FirstClientApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.firstclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
