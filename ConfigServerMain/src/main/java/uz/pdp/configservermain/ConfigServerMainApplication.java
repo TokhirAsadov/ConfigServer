ac7a0f92cc453ba2e906b1d4fddebf88b5487f59
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-03-30 09:37:06 +0500

    Bismillah

---
+++ ConfigServerMain/src/main/java/uz/pdp/configservermain/ConfigServerMainApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.configservermain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerMainApplication.class, args);
+    }
+
+}
---
+++ ConfigServerMain/src/test/java/uz/pdp/configservermain/ConfigServerMainApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.configservermain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
